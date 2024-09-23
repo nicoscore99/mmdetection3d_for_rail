6d92e4094c6505811495422020ececf012dd7a70
stinicol <stinicol@ethz.ch>
2024-09-23 15:04:19 +0200

    small model implementation

--- configs/pointnet_based_classification/pointnet_model_small.py
+++ configs/pointnet_based_classification/pointnet_model_small.py
@@ -3,7 +3,7 @@ model = dict(
     data_preprocessor=dict(
         type='Cls3DDataPreprocessorEvaluation',
         normalize_mean='True',
-        normalize_size='False',
+        normalize_size='True',
         downsample='True',
         num_pts_downsample=256),    
     backbone=dict(
@@ -12,7 +12,7 @@ model = dict(
         num_points=(256, 1),
         radius=(0.2, 0.5),
         num_samples=(32, 64),
-        sa_channels=((64, 64, 128), (128, 128, 256), (256, 512)),
+        sa_channels=((64, 64, 128), (128, 256, 512)),
         fp_channels=(),
         ),
     cls_head=dict(
